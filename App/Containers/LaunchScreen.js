53651174628cd57b1fc22e995ac74d95cbc47270
Braun Goodson <brn.gdsn@gmail.com>
2018-01-04 19:44:07 -0500

    added ignite boilerplate

---
+++ App/Containers/LaunchScreen.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import autoBind from 'react-autobind'
+import { ScrollView, Text, Image, View } from 'react-native'
+// import firebase from 'react-native-firebase';
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+import { Images } from '../Themes'
+
+// Styles
+import styles from './Styles/LaunchScreenStyles'
+
+export default class LaunchScreen extends Component {
+  constructor (props) {
+    super(props)
+    autoBind(this)
+    this.state = {
+      busy: false,
+      username: null,
+      password: null,
+      loggedIn: false,
+      usernameError: false,
+      passwordError: false
+    }
+  }
+  onUsername (username) {
+    return this.setState({
+      username: username
+    })
+  }
+  onPassword (password) {
+    return this.setState({
+      password: password
+    })
+  }
+  async onLogin () {
+    // const { username, password } = this.state
+    // this.setState({
+    //   busy: true
+    // })
+    // const user = await firebase.auth().signInWithEmailAndPassword(username, password)
+    // if (!user) {
+    //   this.setState({
+    //     usernameError: true,
+    //     passwordError: true
+    //   })
+    // } else {
+    //   this.setState({
+    //     user
+    //   })
+    //   this.setState({
+    //     usernameError: false,
+    //     passwordError: false
+    //   })
+    // }
+    // this.setState({
+    //   busy: false
+    // })
+  }
+  render () {
+    const { onUsername, onPassword, onLogin, busy } = this
+    const { usernameError, passwordError } = this.state
+    return (
+      <View style={styles.mainContainer}>
+        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
+        <ScrollView style={styles.container}>
+          <View style={styles.centered}>
+            <Image source={Images.launch} style={styles.logo} />
+          </View>
+          <View style={styles.loginContainer}>
+
+            <FormLabel labelStyle={styles.labelStyle}>Username</FormLabel>
+            <FormInput onChangeText={onUsername} autoCapitalize={'none'} />
+            {usernameError && <FormValidationMessage>No such username.</FormValidationMessage>}
+
+            <FormLabel labelStyle={styles.labelStyle}>Password</FormLabel>
+            <FormInput onChangeText={onPassword} autoCapitalize={'none'} secureTextEntry={true} />
+            {passwordError && <FormValidationMessage>Password incorrect.</FormValidationMessage>}
+
+          </View>
+          <Button
+            title='Login'
+            onPress={onLogin}
+            disabled={busy}
+            loading={busy}
+          />
+        </ScrollView>
+      </View>
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './App/Containers/App.js';
 AppRegistry.registerComponent('RNFirebaseStarter', () => App);
